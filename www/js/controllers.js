5bb7681d82bfca707d621d373919a3f1a85f2bbb
Abhay Amin <pixoloproductions@gmail.com>
2015-02-21 15:40:32 +0530

    put data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -44,6 +44,8 @@ angular.module('starter.controllers', [])
     };
 })
 .controller('productsCtrl', function($scope) {
+    $scope.productdata = [];
+    $scope.productdata = [{"id":"65","cat_id":"48","item_title":"Alpine Lager 12","item_price":"18.99","pro_size":"341 ml","item_desc":"Alpine Lager 12\r\n","pro_unit":"","item_image":"2"},{"id":"92","cat_id":"48","item_title":"BEER","item_price":"41.98","pro_size":"341","item_desc":"Alpine Lager 24\r\n","pro_unit":"","item_image":"2"},{"id":"93","cat_id":"48","item_title":"BEER","item_price":"3.24","pro_size":"473 ml","item_desc":"Alpine Lager 473ml Can\/Canette\r\n","pro_unit":"","item_image":"2"},{"id":"94","cat_id":"48","item_title":"BEER","item_price":"11.35","pro_size":"341 ml","item_desc":"Alpine Lager 6\r\n","pro_unit":"","item_image":"2"},{"id":"95","cat_id":"48","item_title":"BEER","item_price":"17.35","pro_size":"355 ml","item_desc":"Alpine Lager 8 Cans\/Canettes\r\n","pro_unit":"","item_image":"2"},{"id":"96","cat_id":"48","item_title":"BEER","item_price":"22.99","pro_size":"341 ml","item_desc":"Alpine Light 12\r\n","pro_unit":"","item_image":"2"},{"id":"97","cat_id":"48","item_title":"BEER","item_price":"22.99","pro_size":"355 ml","item_desc":"Alpine Light 12 Cans\/Canettes\r\n","pro_unit":"","item_image":"2"},{"id":"98","cat_id":"48","item_title":"BEER","item_price":"40.98","pro_size":"341 ml","item_desc":"Alpine Light 24\r\n","pro_unit":"","item_image":"2"}];
 
 })
 
